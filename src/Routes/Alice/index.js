a0cd90770a0c2f36692330b870203c1addb61bf7
JMustang <eecfredes@gmail.com>
2021-05-09 19:13:42 -0300

    feat:rota POST criada

--- src/Routes/Alice/index.js
+++
@@ -1,6 +0,0 @@
-export default async (fastify, opts) => {
-  const genres = fastify.music();
-  fastify.get("/", async (req, reply) => {
-    return genres;
-  });
-};
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,24 @@
 export default async (fastify, opts) => {
+  const genres = fastify.music();
   fastify.get("/", async (req, reply) => {
-    return "Hello from Routes Page!";
+    return genres;
+  });
+
+  fastify.get("/:id", async (req, reply) => {
+    try {
+      const genre = genres.find((genre) => genre.id === +req.params.id);
+      return genre;
+    } catch (error) {
+      reply.code(404).send("NOT THERE!" + error);
+    }
+  });
+
+  fastify.post("/", async function (req, reply) {
+    const { genre } = req.body;
+    if (!genre) {
+      reply.code(404).send("NOT FOUND!");
+    }
+    const listOfGenres = fastify.music(genre);
+    return listOfGenres;
   });
 };
